30813d76a6478107dfee2d2a74f4dab22fc4d3e1
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-06-07 16:31:07 -0400

    Medium 06-07-2025 2434-using-a-robot-to-print-the-lexicographically-smallest-string

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -2,20 +2,20 @@ class Solution:
     def robotWithString(self, s: str) -> str:
         from collections import Counter
 
-        freq = Counter(s)
-        t = []
-        res = []
-
+        freq = Counter(s)        
+        t = []                  
+        res = []                 
         min_char = 'a'
+
         for c in s:
             t.append(c)
             freq[c] -= 1
 
-          
+            # Advance min_char to the smallest character still remaining in s
             while min_char <= 'z' and freq[min_char] == 0:
                 min_char = chr(ord(min_char) + 1)
 
-            
+            # Pop from `t` while top is <= smallest remaining in `s`
             while t and t[-1] <= min_char:
                 res.append(t.pop())
 
