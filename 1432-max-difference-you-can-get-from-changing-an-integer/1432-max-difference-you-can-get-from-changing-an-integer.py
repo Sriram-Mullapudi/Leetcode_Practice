b034397e4a554445c7a86b91d065a7c710f5e417
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-06-15 16:36:58 -0400

    Medium 06-15-2025 1432-max-difference-you-can-get-from-changing-an-integer

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
@@ -1,19 +1,25 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
         num_str = str(num)
+        
+        # Max part
         for d in num_str:
             if d != '9':
-                max_num = int(num_str.replace(d, '9'))
+                max_num = int(''.join(['9' if ch == d else ch for ch in num_str]))
                 break
         else:
-            max_num = num 
+            max_num = num
+        
+        # Min part
         if num_str[0] != '1':
-            min_num = int(num_str.replace(num_str[0], '1'))
+            d = num_str[0]
+            min_num = int(''.join(['1' if ch == d else ch for ch in num_str]))
         else:
             for d in num_str[1:]:
                 if d != '0' and d != '1':
-                    min_num = int(num_str.replace(d, '0'))
+                    min_num = int(''.join(['0' if ch == d else ch for ch in num_str]))
                     break
             else:
                 min_num = num
+
         return max_num - min_num
