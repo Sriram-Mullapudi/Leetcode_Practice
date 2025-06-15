931b6eefb31d590648e75cf5ccb4a48679a9b5df
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-06-15 16:34:50 -0400

    Medium 06-15-2025 1432-max-difference-you-can-get-from-changing-an-integer

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.py
@@ -6,9 +6,7 @@ class Solution:
                 max_num = int(num_str.replace(d, '9'))
                 break
         else:
-            max_num = num  # already all 9's
-        
-        # For minimum value
+            max_num = num 
         if num_str[0] != '1':
             min_num = int(num_str.replace(num_str[0], '1'))
         else:
