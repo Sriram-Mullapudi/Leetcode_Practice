edace0e4095eeeda8a7b56f83d8817ee9db9fad0
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-06-21 15:50:29 -0400

    Medium 06-21-2025 3085-minimum-deletions-to-make-string-k-special

---
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        freq = list(Counter(word).values())
+        freq.sort()
+        min_del = float('inf')
+        
+        for target in freq:
+            deletions = 0
+            for f in freq:
+                if f < target:
+                    deletions += f  # delete entire group
+                elif f > target + k:
+                    deletions += f - (target + k)  # reduce to max allowed
+            min_del = min(min_del, deletions)
+        
+        return min_del
