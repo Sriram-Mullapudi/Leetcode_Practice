d220c1a210f708eb461dd03d11b398afba0ad494
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-08-11 00:41:57 -0400

    Medium 08-11-2025 2438-range-product-queries-of-powers

---
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        MOD = 10**9 + 7
+
+        # Collect exponents of set bits in ascending order
+        exps = []
+        i = 0
+        while (1 << i) <= n:
+            if (n >> i) & 1:
+                exps.append(i)
+            i += 1
+
+        # Prefix sums of exponents
+        pref = [0]
+        for e in exps:
+            pref.append(pref[-1] + e)
+
+        # Answer queries
+        ans = []
+        for L, R in queries:
+            s = pref[R + 1] - pref[L]
+            ans.append(pow(2, s, MOD))
+        return ans
