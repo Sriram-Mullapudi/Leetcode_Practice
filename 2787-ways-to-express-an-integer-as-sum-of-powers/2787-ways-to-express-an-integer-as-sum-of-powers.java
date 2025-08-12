8d6bc2de7318006e58c3f96f50c28fc3223a4031
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-08-12 01:02:05 -0400

    Medium 08-12-2025 2787-ways-to-express-an-integer-as-sum-of-powers

---
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -0,0 +1,43 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int numberOfWays(int n, int x) {
+        // Precompute powers i^x <= n
+        int[] powers = buildPowers(n, x);
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int p : powers) {
+            for (int s = n; s >= p; s--) {
+                dp[s] += dp[s - p];
+                if (dp[s] >= MOD) dp[s] -= MOD;
+            }
+        }
+        return dp[n];
+    }
+
+    private int[] buildPowers(int n, int x) {
+        java.util.ArrayList<Integer> list = new java.util.ArrayList<>();
+        for (int base = 1; ; base++) {
+            long p = ipow(base, x);
+            if (p > n) break;
+            list.add((int) p);
+        }
+        // Convert to array for faster loop
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < arr.length; i++) arr[i] = list.get(i);
+        return arr;
+    }
+
+    // Integer power (x ≤ 5, n ≤ 300 ⇒ safe)
+    private long ipow(int a, int b) {
+        long res = 1, x = a;
+        while (b > 0) {
+            if ((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+}
