fa1a9bd179247bb9f5709c13345c69efac41e93b
Sriram-Mullapudi <srirammullapudi20@gmail.com>
2025-08-29 01:59:21 -0400

    Medium 08-29-2025 3021-alice-and-bob-playing-flower-game

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -1,6 +1,5 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        # Count odds and evens in [1..n] and [1..m]
         on, en = (n + 1) // 2, n // 2
         om, em = (m + 1) // 2, m // 2
         # Alice wins when x+y is odd
